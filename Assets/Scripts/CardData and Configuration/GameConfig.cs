7e8648fffea33dfd017f5a107aad855c49267226
Cell <68498200+omarAlkaley@users.noreply.github.com>
2025-12-09 05:10:15 +0200

    Created the game configuration and the game data

---
+++ Assets/Scripts/CardData and Configuration/GameConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class GameConfig
+{
+	public int rows = 4;
+	public int cols = 4;
+	public float cardSpacing = 0.2f;
+	public Vector2 containerSize = new Vector2(10f , 10f);
+}
